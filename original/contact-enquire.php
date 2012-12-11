2518817edad3fb17c25335073f6a7e822d57036b
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2012-12-11 15:48:42 -0500

    original files

---
+++ original/contact-enquire.php
@@ -0,0 +1,374 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link rel="icon" type="image/png" href="favicon.png">
+<title>Beauty Care  &raquo; Contact</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/style.css" class="cssfx">
+<link rel="stylesheet" type="text/css" href="css/pink.css" class="cssfx">
+<script src="js/cssfx.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/coda-slider-2.0.css" class="cssfx">
+<!-- Phone -->
+<link href="css/phone.css" rel="stylesheet" type="text/css" media="only screen and (max-width:320px)" class="cssfx">
+<!-- Phone Wide -->
+<link href="css/phone-landscape.css" rel="stylesheet" type="text/css" media="only screen and (min-width:321px) and (max-width:480px)" class="cssfx">
+<!-- Tablet -->
+<link href="css/tablet.css" rel="stylesheet" type="text/css" media="only screen and (min-width:481px) and (max-width:768px)" class="cssfx">
+<!-- Desktop -->
+<link href="css/desktop.css" rel="stylesheet" type="text/css" media="only screen and (min-width:769px) and (max-width:1000px)" class="cssfx">
+<!--[if (lt IE 9)&(!IEMobile 7)]>
+      <link rel="stylesheet" href="desktop.css">
+    <![endif]-->
+<link rel="stylesheet" type="text/css" href="css/coda-slider-2.0.css">
+<!--[if IE 9]>
+<link rel="stylesheet" href="css/ie.css">
+<![endif]-->
+<script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+<p id="back-top"><a href="#top"><span></span></a></p>
+
+<!-- Start Wrapper -->
+<div class="wrapper subPage topBorder">
+<!-- Start Header -->
+  <header id="mainHeader" class="clearfix">
+    <div id="logoCon">
+    <!-- Logo -->
+      <h1 id="logo"><a href="index.htm">Beauty <span class="gery">Care</span></a></h1>
+      <p class="des">Tagline Goes Here</p>
+    </div>
+    <!-- Start Main Navigation -->
+    <nav id="menu">
+      <h2 class="visuallyhidden">Main Navigation</h2>
+      <ul class="menu">
+        <li><a href="index.htm">Home</a><span class="desc">welcome</span></li>
+        <li><a href="about.htm">About</a><span class="desc">our company</span></li>
+        <li><a href="#" class="parent">Pages</a><span class="desc">list of Pages</span> 
+          <!-- Dropdown Level 1 -->
+          <ul>
+            <li><a href="#" class="parent">Home Pages</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="index.htm"><span>Home 1</span></a></li>
+                <li><a href="index2.htm"><span>Home 2</span></a></li>
+              </ul>
+            </li>
+            <li><a href="#" class="parent">Sub Page Headers</a>
+              <!-- Dropdown Level 2 -->	
+              <ul>
+                <li><a href="style1.htm"><span>Style 1</span></a></li>
+                <li><a href="style2.htm"><span>Style 2</span></a></li>
+              </ul>
+            </li>
+            <li><a href="about.htm">About</a></li>
+            <li><a href="services.htm" class="parent">Services</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="services.htm"><span>List of Services 1</span></a></li>
+<li><a href="services2.htm"><span>List of Services 2</span></a></li>
+                <li><a href="service-single.htm"><span>Service and Packages</span></a></li>
+              </ul>
+            </li>
+            <li><a href="products.htm" class="parent">Products</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="products.htm">All Products</a></li>
+                <li><a href="product-single.htm">About a Product</a></li>
+              </ul>
+            </li>
+            <li><a href="#" class="parent">Others</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="left-navigation.htm">Left Navigation</a></li>
+                <li><a href="right-navigation.htm">Right Navigation</a></li>
+                <li><a href="left-sidebar.htm">Left Sidebar</a></li>
+                <li><a href="right-sidebar.htm">Right Sidebar</a></li>
+                <li><a href="full-width.htm">Full Width</a></li>
+              </ul>
+            </li>
+            <li><a href="blog.htm" class="parent">Blog</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="blog.htm">Blog</a></li>
+                <li><a href="single-blog.htm">Single Blog</a></li>
+              </ul>
+            </li>
+            <li><a href="contact-enquire.php" class="parent">Contact</a> 
+              <!-- Dropdown Level 2 -->
+              <ul>
+                <li><a href="contact-enquire.php">Enquire</a></li>
+                <li><a href="contact-appointment.php">Appointment</a></li>
+              </ul>
+            </li>
+          </ul>
+          <!-- Dropdown Level 1 End --> 
+        </li>
+        <li><a href="services.htm">Services</a><span class="desc">what we do</span></li>
+        <li><a href="blog.htm">Blog</a><span class="desc">articles</span></li>
+        <li class="last current"><a href="contact-enquire.php">Contact</a><span class="desc">Need Help?</span></li>
+      </ul>
+    </nav>
+    <!-- End of Main Navigation --> 
+  </header>
+  <!-- End of Header -->
+  
+  
+  <div class="circleSep">
+    <div class="circleLeft"></div>
+    <div class="circleRight"></div>
+    <div class="sep"></div>
+  </div>
+  
+  
+  <!-- Start Sub page Banner -->
+  <div id="bannerWide" class="subBanner"> <img src="img/sub-banner.png" alt="" class="bannerBg">
+    <div class="bannerCon">
+      <h2>Enquire</h2>
+      <p><a href="index.htm">Home</a> » <a href="contact-enquire.php">Enquire</a></p>
+    </div>
+    <div class="searchCon">
+      <form>
+        <input name="search" type="text" class="textField" placeholder="search">
+        <input name="submitSearch" type="submit" class="searchBtn">
+      </form>
+    </div>
+  </div>
+  <!-- End of Sub Page Banner -->
+  
+  <div class="sep"></div>
+  
+  <!-- Start Main Container -->
+  <div id="mainContainer">
+    <div id="mainContent" class="contact enquire">
+      <h2 class="title"><span class="blue">NEED </span>HELP?</h2>
+      <p>Ut fringilla. Aliquam feugiat pede vel nisl. Fusce rhoncus. Praesent sed mi a dui tempor vulputate. Phasellus consectetuer nulla id metus. Nam magna odio, venenatis non, venenatis quis, iaculis eget, mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nunc ante nibh, porttitor quis.</p>
+      <!-- Map -->
+      <div class="map">
+        <iframe width="100%" height="296" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=New+York,+NY,+USA&amp;aq=0&amp;oq=ne&amp;sll=37.0625,-95.677068&amp;sspn=37.735377,86.044922&amp;ie=UTF8&amp;hq=&amp;hnear=New+York&amp;t=p&amp;z=11&amp;ll=40.714353,-74.005973&amp;output=embed"></iframe>
+      </div>
+      <div class="sep"></div>
+      <div class="clearfix columns noBorder noMarginBottom">
+      
+      <!-- Contact Form -->
+        <article class="two_third">
+          <h2><span class="blue">Enqire</span> Form</h2>
+          <?php
+			 if(isset($_POST['submit'])){
+				 
+				 $name = $_POST['name'];
+				 $email = $_POST['email'];
+				 $subject = $_POST['subject'];
+				 $message = $_POST['message'];
+				 $success= "Thanks for contact us. We will be get in touch shortly.";
+				 
+				 $error = 0;
+				 $error_info = "<span class=\"formContactText\">The following error has been occured</span><br>";
+				 
+				 if(empty($name)){
+					 $error = 1;
+					 $error_info .= "<span class=\"formContactText\">Name Field is Empty</span><br>";
+					 }
+				if(empty($email)){
+					 $error = 1;
+					 $error_info .= "<span class=\"formContactText\">Email Field is Empty</span><br>";
+					 }
+				if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,6}$", $email))
+						{
+						$error = 1;
+						$error_info .= "<span class=\"formContactText\">The Email address that you entered appears to be invalid<br></span>";
+						} 
+				if(empty($subject)){
+					 $error = 1;
+					 $error_info .= "<span class=\"formContactText\">Subject Field is Empty</span><br>";
+					 }	 
+				if(empty($message)){
+					 $error = 1;
+					 $error_info .= "<span class=\"formContactText\">Message Field is Empty</span>";
+					 }	 
+				if($error == 1){
+					echo "<p>$error_info</p>";
+					}
+						 
+				 if($error==0){
+					$to = "falconcreativestudio@gmail.com"; 
+					
+					$details = "Name: ".$name."\n\n";
+					$details .= "Email: ".$email."\n\n";
+					$details .= "Subject: ".$subject."\n\n";
+					$details .= "Message: \n".$message."\n\n";
+					$body .= $details;
+					
+					
+					$headers = "From: $name <$email> \r\n";
+					$headers .= "X-Mailer: Info From Beaty Care site Template\r\n";
+			
+					
+					mail($to, $subject, $body, $headers);
+					echo '<p class="orange">' . $success . '</strong></p>';
+					 }
+				 }
+			 ?>
+          <form method="post">
+            <p>
+              <input name="name" type="text" class="textField" placeholder="Name*" required>
+            </p>
+            <p>
+              <input name="email" type="email" class="textField" placeholder="Email*" required>
+            </p>
+            <p>
+              <input name="subject" type="text" class="textField" placeholder="Subject*" required>
+            </p>
+            <p>
+              <textarea name="message" placeholder="Comment*"  class="textArea" required></textarea>
+            </p>
+            <p>
+              <input name="submit" type="submit" class="submitBtn" value="Submit">
+            </p>
+          </form>
+        </article>
+        
+        <!-- Contact Address -->
+        <article class="one_third last">
+          <h2><span class="blue">Contact</span> Address</h2>
+          <p>Beauty Care Treatment Center,<br>
+            15 Mercer Street,<br>
+            New York, NY </p>
+          <p>(212) 564 - 5542</p>
+          <p> info@beauty-care.com </p>
+        </article>
+      </div>
+      
+      <div class="sep"></div>
+      <article class="clearfix columns noBorder beInTouch">
+        <h2>BE IN <span class="blue">TOUCH</span></h2>
+        <article class="two_third noBorder">
+          <h3>What Our <span class="blueBlod">Customer Says</span></h3>
+          
+          <!-- Testimonials -->
+          <div id="testimonial">
+            <div class="coda-slider" id="slider-id">
+        <div>
+          <h2 class="title">Panel 1</h2>
+          <blockquote>
+                    <p>Nullam eu ante. Sed tempor dolor. Duis ut neque eu nulla consequat semper. Donec malesuada. Pellentesque cursus mauris sit amet nisl. Nullam aliquet tellus ac ipsum. Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Praesent mauris massa, imperdiet ornare, venenatis eget, fringilla ut, tellus.</p>
+                    <p class="author">- JHON DEO</p>
+                  </blockquote>
+        </div>
+        <div>
+          <h2 class="title">Panel 2</h2>
+          <blockquote>
+                    <p>Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Nullam eu ante. Sed tempor dolor. Duis ut neque eu nulla consequat semper. Donec malesuada. Pellentesque cursus mauris sit amet nisl. Nullam aliquet tellus ac ipsum. Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Praesent mauris massa, imperdiet ornare, venenatis eget, fringilla ut, tellus.</p>
+                    <p class="author">- JHON DEO</p>
+                  </blockquote>
+        </div>
+        <div>
+          <h2 class="title">Panel 3</h2>
+          <blockquote>
+                    <p>Donec malesuada. Pellentesque cursus mauris sit amet nisl. Nullam aliquet tellus ac ipsum. Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Praesent mauris massa, imperdiet ornare, venenatis eget, fringilla ut, tellus.</p>
+                    <p class="author">- JHON DEO</p>
+                  </blockquote>
+        </div>
+        <div>
+          <h2 class="title">Panel 4</h2>
+          <blockquote>
+                    <p>Pellentesque cursus mauris sit amet nisl. Nullam aliquet tellus ac ipsum. Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Praesent mauris massa, imperdiet ornare, venenatis eget, fringilla ut, tellus. Nullam eu ante. Sed tempor dolor. Duis ut neque eu nulla consequat semper. Donec malesuada. Pellentesque cursus mauris sit amet nisl. Nullam aliquet tellus ac ipsum. Maecenas suscipit commodo diam. Curabitur imperdiet. Fusce porta. Aliquam a risus. Praesent mauris massa, imperdiet ornare, venenatis eget, fringilla ut, tellus.</p>
+                    <p class="author">- JHON DEO</p>
+                  </blockquote>
+        </div>
+      </div>
+            <div class="arrow"></div>
+          </div>
+        </article>
+        
+        <!-- Recent Tweets -->
+        <article class="one_third last recentTweet">
+          <h3>Recent <span class="blueBlod">Tweets</span></h3>
+          <div id="twitter_update_list"></div>  
+        </article>
+      </article>
+    </div>
+    
+  </div>
+  <div class="circleSep">
+    <div class="circleLeft"></div>
+    <div class="circleRight"></div>
+    <div class="sep"></div>
+  </div>
+  
+  <!-- Start Footer -->
+  <footer id="pageFooter" class="clearfix">
+    <h2 class="visuallyhidden">Footer Widgets</h2>
+    <div class="one_fourth widget">
+      <h3>ABOUT US</h3>
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque nisi. Etiam et risus. Proin auctor. Proin at pede at libero dapibus euismod. Maecenas massa erat,</p>
+    </div>
+    <div class="one_fourth widget">
+      <h3>SERVICES</h3>
+      <ul>
+        <li><a href="#">Massage</a></li>
+        <li><a href="#">Facial and Hair Treatments</a></li>
+        <li><a href="#">Skin Care</a></li>
+        <li><a href="#">Body Treatments &amp; Massage</a></li>
+        <li><a href="#">Nail</a></li>
+        <li><a href="#">Spa Rest</a></li>
+      </ul>
+    </div>
+    <div class="one_fourth widget">
+      <h3>CONTACT DETAILS</h3>
+      <ul class="address-details">
+        <li class="name">John Deo</li>
+        <li class="address">Beauty Care Treatment Center,<br>
+          15 Mercer Street,<br>
+          New York, NY</li>
+        <li class="phone">(212) 564 - 5542</li>
+        <li class="email"><a href="mailto:info@spacare.com" target="_blank">info@beauty-care.com</a></li>
+      </ul>
+    </div>
+    <div class="one_fourth last widget">
+      <h3>BLOG CATEGORY</h3>
+      <ul>
+        <li><a href="#">Massage</a></li>
+        <li><a href="#">Facial and Hair Treatments</a></li>
+        <li><a href="#">Skin Care</a></li>
+        <li><a href="#">Body Treatments &amp; Massage</a></li>
+        <li><a href="#">Nail</a></li>
+        <li><a href="#">Spa Rest</a></li>
+      </ul>
+    </div>
+  </footer>
+  <!-- End of Footer -->
+</div>
+<!-- End of Wrapper -->
+
+<!-- Copyright -->
+<div class="clearfix" id="copyrightText">
+  <p class="floatLeft">&copy; 2012 <a href="index.htm">beauty-care.com</a>, All Rights Reserved. </p>
+  <p class="floatRight socialIcons"> Follow Us: <a href="https://plus.google.com/113423345865351724434" title="" target="_blank" class="google">google plus</a><a href="#" title="" class="rss">rss</a><a href="http://www.facebook.com/falcon4web" title="" target="_blank" class="facebook">facebook</a><a href="https://twitter.com/falcon_studio" title="" target="_blank" class="twitter">twitter</a></p>
+</div>
+<!-- End of Copyright -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script> 
+<script src="js/plugins.js"></script> 
+<script src="js/script.js"></script> 
+<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js">  
+</script>  
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/falcon_studio.json?callback=twitterCallback2&count=2">  </script>
+<script>
+/*	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));*/
+</script>
+</body>
+</html>
