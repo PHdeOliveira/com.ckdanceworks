8ec6f66b3cd58bbf4a5767aa4bbc4d81a623829d
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2012-12-19 14:53:30 -0500

    db user change

--- beta/wp-config.php
+++ beta/wp-config.php
@@ -19,7 +19,7 @@
 define('DB_NAME', 'ckdancew_newsite');
 
 /** MySQL database username */
-define('DB_USER', 'ckroot');
+define('DB_USER', 'ckdancew_newsite');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'ckroot');
