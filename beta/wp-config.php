4cdcf036ff0fa7bdbe2fc6a4268808c72de5d7d1
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2012-12-19 14:46:55 -0500

    db name changes

--- beta/wp-config.php
+++ beta/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ckdatabase');
+define('DB_NAME', 'ckdancew_newsite');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ckroot');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'ckroot');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '173.254.28.139');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
