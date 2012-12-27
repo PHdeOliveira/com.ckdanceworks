b7b0a88ec4ec09d0dee2f62ecc8296b0887a59e1
Pedro Oliveira <pedro.hrde.oliveira@gmail.com>
2012-12-27 13:34:51 -0500

    database sync

--- beta/wp-config.php
+++ beta/wp-config.php
@@ -27,7 +27,7 @@ define('DB_USER', 'ckdancew_test');
 define('DB_PASSWORD', '1234!');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'just139.justhost.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
